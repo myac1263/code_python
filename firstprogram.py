c4f7f696591c64ced1dcda740858ed62af49f78b
myac1263 <myac1263@gmail.com>
2024-09-20 09:42:26 -0400

    edited

---
+++ firstprogram.py
@@ -0,0 +1,7 @@
+# Basic Calculator
+
+print("Hey Fam, welcome to my bussin Calculator")
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+answer = num1 + num2
+print(f"Your answer is: {answer}")
