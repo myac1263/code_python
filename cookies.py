bfe22398ed6c76e3b3f3bde26d138a9dfdef1bf2
myac1263 <myac1263@gmail.com>
2024-09-23 09:40:27 -0400

    edited

---
+++ cookies.py
@@ -0,0 +1,12 @@
+# Goal: for the program to output the total cookies sold
+
+# input
+start_money = float(input()) # float() helps to cast its argument to a real number
+cookies_sold = input()
+
+# processing
+big_cookies = 0 # this is an example of variable initialization
+cookies = 0 
+
+for _ in cookies_sold: 
+
