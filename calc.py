85eb688b4a9d5b608147bfae9be1c3b01dfee62f
myac1263 <myac1263@gmail.com>
2024-09-23 08:45:54 -0400

    finished

---
+++ calc.py
@@ -0,0 +1,15 @@
+# Input Handling
+number1 = int(input("Enter number 1: "))
+number2 = int(input("Enter number 2: "))
+
+# Operations
+result1 = number1 + number2
+result2 = number1 - number2
+result3 = number1 * number2
+result4 = number1 / number2
+
+# Output
+print(f"{number1} + {number2} = {result1}")
+print(f"{number1} - {number2} = {result2}")
+print(f"{number1} * {number2} = {result3}")
+print(f"{number1} / {number2} = {result4}")
