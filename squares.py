cacefc54a74af7422214cdf86e8b1f1b9222e999
myac1263 <myac1263@gmail.com>
2024-12-26 12:15:38 -0500

    redid for practice

--- squares.py
+++ squares.py
@@ -1,14 +1,10 @@
-# import statments
-from math import sqrt, floor
+# Write a program that inputs the number of tiles and then prints out the maximum side length. 
+# You may assume that the number of tiles is less than ten thousand. 
 
-#input
-titles = int(input()) #int() is a type casting function that converts its argument to integers
+import math
 
-# processing
+num_tiles = int(input("Input the number of tiles: ")) # int is a typecasting function
+side_length =  math.floor(math.sqrt(num_tiles)) #floor rounds a number down to the nearest integer less than or equal to the number
+#placing math. infront of the math modulus things (floor, sqrt ...)
 
-# answer = titles ** 0.5
-answer = sqrt(titles)
-answer = floor(answer)
-
-#output
-print(f"The largest square had a side length {answer}.")
+print (f"The largest square has side length {side_length}.")
