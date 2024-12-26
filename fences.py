7599b3ab5d5eeb3098b49c07cc469917e3b837b1
myac1263 <myac1263@gmail.com>
2024-12-25 22:51:43 -0500

    redid for practice

--- fences.py
+++ fences.py
@@ -1,30 +1,21 @@
-#Sample Input:
-#FF
-#FFFF
-#FFFFFF
+# You are to create a program that determines how many paint cans we would need.
+# Each plank of the fence requires one paint can.
+# Your supplier only sells paint cans by the dozen (12) for $14.95.
+# There are 3 fenced sections, and their length will be denoted by a series of ‘F’ for each fence plank.
+# Output how many cans you’d need, how many paint cans you have leftover, and how much it would cost.
 
-#Sample Output:
-#12
-#0
-#14.95
-
-# import statement
 from math import ceil
 
-#input 
-section1 = input("Enter 1: ")
-section2 = input("Enter 2: ")
-section3 = input("Enter 3: ")
+section_one = input("How many fence planks are there in section one:")
+section_two = input("How many fence planks are there in section two:")
+section_three = input("How many fence planks are there in section three:")
+print(f'{section_one} {section_two} {section_three}')
 
-#processing
-cans = len(section1) + len(section2) + len(section3)
+cans = len(section_one) + len(section_two) + len(section_three) #each length would be the number of fences in that section
 
-boxes = cail(cans / 12)
+boxes = ceil(cans / 12) #ceil allows me to round up
 leftover = 12*boxes - cans
 
-total_cost = 14,95 * boxes 
+total_cost = 14.95 * boxes
 
-# output 
-print(f"We need {cans} paint cans.")
-print(f"We have {leftover} leftover")
-print(f"The project costs ${total_costs}.")
+print(f"We need {cans} of cans. We would have {leftover} leftovers and it would cost us {total_cost} dollars")
